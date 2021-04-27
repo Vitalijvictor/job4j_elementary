b8db254d49c347712c1c21dbeb5afe8554fc265e
VitalijVictor <vitalijvictor@gmail.com>
2021-04-27 16:12:14 +0200

    InfoClass for GitHub

--- src/ru/job4j/InfoClass.java
+++ src/ru/job4j/InfoClass.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class InfoClass {
 
 	public static void main(String[] args) {
-		System.out.println("01/01/1976");
+		System.out.println(" date of birth 01/01/1976");
 	}
 
 }
