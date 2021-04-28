579252eed0a9abe1577e1e482490f633cf7e9cba
VitalijVictor <vitalijvictor@gmail.com>
2021-04-28 18:47:41 +0200

    Greeting Checkstyle

--- src/condition/Greeting.java
+++ src/condition/Greeting.java
@@ -7,6 +7,7 @@ public class Greeting {
 		int year = 2021;
 		idea = idea + year;
 		System.out.println(idea +  " But I am a newbie.");
+		System.out.println(year +  " But I am a newbie.");
 	}
 
 }
