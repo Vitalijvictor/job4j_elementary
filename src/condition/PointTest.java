fffe29e8cbb4f9e1ed302e5b71ae73f4a58bf009
VitalijVictor <vitalijvictor@gmail.com>
2021-04-29 21:05:00 +0200

    PointTest  версия 2

--- src/condition/PointTest.java
+++ src/condition/PointTest.java
@@ -15,8 +15,8 @@ public class PointTest {
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
-    public void when10to20then2() {
-        int expected = 2;
+    public void when10to20then1() {
+        int expected = 1;
         int x1 = 1;
         int y1 = 0;
         int x2 = 2;
@@ -25,22 +25,22 @@ public class PointTest {
         Assert.assertEquals(expected, out, 0.01);
     }
     @Test
-    public void when11to42then2() {
-        int expected = 2;
-        int x1 = 1;
-        int y1 = 1;
+    public void when20to26then6() {
+        int expected = 6;
+        int x1 = 2;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 6;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+    @Test
+    public void when42to416then14() {
+        int expected = 14;
+        int x1 = 4;
+        int y1 = 2;
         int x2 = 4;
-        int y2 = 2;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-    @Test
-    public void when01to20then2() {
-        int expected = 2;
-        int x1 = 1;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        int y2 = 16;
         double out = Point.distance(x1, y1, x2, y2);
         Assert.assertEquals(expected, out, 0.01);
     }
