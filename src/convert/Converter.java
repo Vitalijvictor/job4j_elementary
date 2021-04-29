f1f6c5e9b6cecca28541ec99490fef1bfa8b6e0b
VitalijVictor <vitalijvictor@gmail.com>
2021-04-29 16:27:12 +0200

    Converter

---
+++ src/convert/Converter.java
@@ -0,0 +1,32 @@
+package convert;
+
+public class Converter {
+
+    public static double rubleToEuro(double value) {
+        
+    	return value / 70;
+    }
+
+    public static double rubleToDollar(double value) {
+        
+    	return value / 60;
+    }
+
+    public static void main(String[] args) {
+    	double euro = Converter.rubleToEuro(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        
+        double dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + dollar + " dollar.");
+        
+    	double in = 140;
+    	double expected = 2;
+    	double out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+    }
+}
