0e6dcf69811b757a302cd139f9dcc78433502576
VitalijVictor <vitalijvictor@gmail.com>
2021-05-03 13:43:35 +0200

    MaxTest Version 2

--- test/condition/MaxTest.java
+++ test/condition/MaxTest.java
@@ -22,10 +22,26 @@ public class MaxTest {
     }
     @Test
     public void whenMax1To1Then1() {
-        int left = 1;
+        int left = 3;
         int right = 1;
         int result = Max.max(left, right);
-        int expected = 1;
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+    @Test
+    public void whenMax3To6Then6() {
+        int left = 3;
+        int right = 6;
+        int result = Max.max(left, right);
+        int expected = 6;
+        Assert.assertEquals(result, expected);
+    }
+    @Test
+    public void whenMax7To7Then7() {
+        int left = 7;
+        int right = 7;
+        int result = Max.max(left, right);
+        int expected = 7;
         Assert.assertEquals(result, expected);
     }
 }
