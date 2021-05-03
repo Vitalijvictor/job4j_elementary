9506a62961a91e0462d9b5f83e3fd0ddcee53964
VitalijVictor <vitalijvictor@gmail.com>
2021-05-03 19:11:52 +0200

    ThreeMax Version 2

--- src/condition/ThreeMax.java
+++ src/condition/ThreeMax.java
@@ -2,16 +2,8 @@ package condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = first;
-        if (third > second && third > first) {
-            result = first;
-        }
-        if (first > second && first > third) {
-            result = second;
-        }
-        if (second > first && second > third) {
-            result = third;
-        }
+        int result = (first > second && first > third) ? first : ((second > third ? second : third));
+        
         return result;
     }
 }
