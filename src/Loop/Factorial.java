15c73c32af0f06fe54830941dd06e92e309106e4
VitalijVictor <vitalijvictor@gmail.com>
2021-05-04 10:44:47 +0200

    Factorial version 2

--- src/Loop/Factorial.java
+++ src/Loop/Factorial.java
@@ -3,8 +3,9 @@ package Loop;
 public class Factorial {
     public static int calc(int n) {
         int result = 0;
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++) {
         result= result * i;
+        }
         return result;
     }
 }
