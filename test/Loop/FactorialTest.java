27a45b2334ff6c6184ecd91652da9972c04bf0b6
VitalijVictor <vitalijvictor@gmail.com>
2021-05-04 14:42:12 +0200

    FactorialTest version 2

--- test/Loop/FactorialTest.java
+++ test/Loop/FactorialTest.java
@@ -7,12 +7,16 @@ public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
         int n = 5;
-        int result = 120;
+    	int expected = 120;
+        int result = Factorial.calc(n);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenCalculateFactorialForZeroThenOne() {
-    	int n = 0;
-        int result = 1;
+    public void whenCalculateFactorialForOneThenOne() {
+        int n = 1;
+    	int expected = 1;
+        int result = Factorial.calc(n);
+        Assert.assertEquals(expected, result);
     }
 }
