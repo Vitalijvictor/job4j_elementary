62374e9cd471108f6d1f218b74f90fd7185e0b6e
VitalijVictor <vitalijvictor@gmail.com>
2021-05-04 10:34:24 +0200

    FactorialTest

---
+++ test/Loop/FactorialTest.java
@@ -0,0 +1,18 @@
+package Loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactorialTest {
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int n = 5;
+        int result = 120;
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+    	int n = 0;
+        int result = 1;
+    }
+}
