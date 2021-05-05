41ad1e2ed45eb88ce0417f4f92a9902d09955ea7
VitalijVictor <vitalijvictor@gmail.com>
2021-05-05 20:34:18 +0200

    CheckPrimeNumber version2

--- src/Loop/CheckPrimeNumber.java
+++ src/Loop/CheckPrimeNumber.java
@@ -3,7 +3,7 @@ package Loop;
 	public class CheckPrimeNumber {
 	    public static boolean check(int number) {
 	        boolean prime = number > 1;
-	        for (int index = 2; index > number;) {
+	        for (int index = 2; index < number; index++) {
 				if ((number % index) == 0){
 	        		prime = false;
 	        		break;
