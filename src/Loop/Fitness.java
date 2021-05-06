0137e9766874adb577d052565e503716dc5a21b0
VitalijVictor <vitalijvictor@gmail.com>
2021-05-06 18:20:21 +0200

    Fitness version2

--- src/Loop/Fitness.java
+++ src/Loop/Fitness.java
@@ -3,11 +3,11 @@ package Loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while ( ivan < nik) {
+        while ( nik >= ivan) {
         	ivan *= 3;
         	nik *=2;
         	month++;
         }
-        return month;
+		return month;
     }
 } 
