84f601f8e57947ee3131c6b64a54f70f0a43ed81
VitalijVictor <vitalijvictor@gmail.com>
2021-05-07 18:19:45 +0200

    ShessBoard version2

--- src/condition/ChessBoard.java
+++ src/condition/ChessBoard.java
@@ -10,22 +10,33 @@ public class ChessBoard {
         return rsl;
     }
     
-    private static boolean inRange(int z) {
-    	return (z < 0 || z > 7); 
-    } 
+//    private static boolean notInRange(int z) {
+//    	return (z < 0 || z > 7); 
+//    } 
+//    
+//    private static boolean inRange(int z) {
+//    	return (z >= 0 && z <= 7); 
+//    }
     
     public static int way(int x1, int y1, int x2, int y2) {
-
-    	if (inRange(x1) || inRange(y1) || inRange(x2) || inRange(y2)) 
-    		return 0;
-    	int diffX = Math.abs(x1 - x2);
-    	int diffY = Math.abs(y1 - y2);
-    
-    	if (diffX == diffY) {
-    		return diffX;
+    	if ((x1 >= 0 && x1 <= 7) && (y1 >= 0 && y1 <= 7) && (x2 >= 0 && x2 <= 7) && (y2 >= 0 && y2 <= 7)) {
+    		int diffX = Math.abs(x1 - x2);
+        	int diffY = Math.abs(y1 - y2);
+        
+        	if (diffX == diffY) {
+        		return diffX;
+        	}	
     	}
+    	
+//    	if (inRange(x1) && inRange(y1) && inRange(x2) && inRange(y2)) {
+//    		int diffX = Math.abs(x1 - x2);
+//        	int diffY = Math.abs(y1 - y2);
+//        
+//        	if (diffX == diffY) {
+//        		return diffX;
+//        	}
+//    	}
+    	
     	return 0;
     }
-    
-    
 }
