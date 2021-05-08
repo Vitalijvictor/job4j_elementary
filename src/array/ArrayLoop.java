c3d18ff551170563e338f374528cd851e75ab3c8
VitalijVictor <vitalijvictor@gmail.com>
2021-05-08 08:51:50 +0200

    ArrayLoop

---
+++ src/array/ArrayLoop.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ArrayLoop {
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		for (int index = 0; index < numbers.length; index++) {
+		int	y = index * 2 + 3;
+		System.out.println(y);
+	}	
+}
+}
