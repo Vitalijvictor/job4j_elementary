93595f435c0aa2824ccba9f0a5198eef0f9a3c21
VitalijVictor <vitalijvictor@gmail.com>
2021-05-08 14:37:50 +0200

    changes after name packages

--- src/Loop/LoopFor.java
+++
@@ -1,9 +0,0 @@
-package Loop;
-
-public class LoopFor {
-    public static void main(String[] args) {
-        for (int index = 5; index <= 10; index++) {
-            System.out.println(index);
-        }
-    }
-}
