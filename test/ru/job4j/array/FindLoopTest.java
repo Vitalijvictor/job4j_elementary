f112c677532f944a3c388a95c429bb9d030b4efa
VitalijVictor <vitalijvictor@gmail.com>
2021-05-09 22:32:19 +0200

    FindLoopTest version2

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -18,7 +18,7 @@ public class FindLoopTest {
         int[] data = {3, 2, 1, 0};
         int el = 4;
         int result = FindLoop.indexOf(data, el);
-        int expected = 0;
+        int expected = -1;
         Assert.assertEquals(expected, result);
     }
 }
