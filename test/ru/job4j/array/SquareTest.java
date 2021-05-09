0ad9647a7bdc5a1cfb7e813958f12717555bc6ab
VitalijVictor <vitalijvictor@gmail.com>
2021-05-09 22:30:06 +0200

    SquareTest version2

--- test/ru/job4j/array/SquareTest.java
+++ test/ru/job4j/array/SquareTest.java
@@ -16,7 +16,7 @@ public class SquareTest {
     public void whenBound5Then014() {
         int bound = 5;
         int[] result = Square.calculate(bound);
-        int[] expected = new int[] {0, 1, 4};
+        int[] expected = new int[] {0, 1, 4, 9, 16};
         Assert.assertArrayEquals(expected, result);
     }
 }
