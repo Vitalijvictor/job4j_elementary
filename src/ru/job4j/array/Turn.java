0b4b0a23ea71e2750afc8722b4a517326d5459a4
VitalijVictor <vitalijvictor@gmail.com>
2021-05-10 11:18:54 +0200

    Turn
    wrong version

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+    	for (int index = 0; index < array.length; index++) {
+			int temp = array[index];
+			array[index] = array[array.length - index - 1];
+			array[array.length - index - 1] = array[index];
+			array[index] = temp;
+		}
+	return array;
+}
+}
