3eec33ce970b55f695bd07370ea9fcd20f5bc414
VitalijVictor <vitalijvictor@gmail.com>
2021-05-11 10:17:39 +0200

    FindLoop
    Version 2

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -14,9 +14,10 @@ package ru.job4j.array;
     
     public static int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; 
-        	for (int i = 0; start < finish; i++) {
+        	for (int i = start; i <= finish; i++) {
         		if (data[i] == el) {
         			rst = i;
+        			break;
         		}
         	}
         return rst;
