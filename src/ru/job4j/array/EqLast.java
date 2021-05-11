5730ba7e9f2c1ad08af65a44c912580590cdc60a
VitalijVictor <vitalijvictor@gmail.com>
2021-05-11 20:57:45 +0200

    EqLast
    version 3

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -2,10 +2,7 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-    	return false;
-    	if (left[left.length - 1] == right[right.length - 1]) {
-    		return true;
-    	}
+    	return (left[left.length - 1] == right[right.length - 1]);
     }
- }
+}
     
