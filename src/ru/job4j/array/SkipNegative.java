2fbb39861e6a6ec29167b67a3f05e74e8b263c12
VitalijVictor <vitalijvictor@gmail.com>
2021-05-12 08:39:50 +0200

    SkipNegative version2

--- src/ru/job4j/array/SkipNegative.java
+++ src/ru/job4j/array/SkipNegative.java
@@ -4,12 +4,11 @@ public class SkipNegative {
     public static int[][] skip(int[][] array) {
     	for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array.length; cell++) {
-                int val = array[row][cell];
-                if (val < 0 ) {
-                	val == 0;
-                }
-        return array;
-    }
-}
+                if (array[row][cell] <  0) {
+                	array[row][cell] = 0;
+                 }  
+             }
+    	}
+		return array;
     }
 }
