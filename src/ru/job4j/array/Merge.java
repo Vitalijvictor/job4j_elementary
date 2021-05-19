983cdaed835594bfebffd9c68efeda9425175358
VitalijVictor <vitalijvictor@gmail.com>
2021-05-19 19:31:26 +0200

    Merge
    Version2

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -1,22 +1,20 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        for (int i = 0; i < right.length; i++) {
-            for (int j = 0; j < left.length; j++) {
-                if (left[i] > right[j]) {
-                    int temp = left[i];
-                    left[i] = left[j];
-                    left[j] = temp;
-                }
-                else {
-                   int temp = right[j];
-                    right[j] = right[i];
-                    right[i] = temp;
-                }
-            }
+        int mainIndex = 0;
+
+        for (int j = 0; j < left.length; j++) {
+            rsl[mainIndex++] = left[j];
         }
+        for (int i = 0; i < right.length; i++) {
+            rsl[mainIndex++] = right[i];
+        }
+
+        Arrays.sort(rsl);
         return rsl;
     }
 }
