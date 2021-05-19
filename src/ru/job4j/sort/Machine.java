96ff6cd08f38c4e120ad4d3d05f41e8eff4f4d3b
VitalijVictor <vitalijvictor@gmail.com>
2021-05-19 21:16:48 +0200

    Machine
    final

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -2,18 +2,30 @@ package ru.job4j.sort;
 
 import java.util.Arrays;
 
-import org.junit.internal.runners.model.EachTestNotifier;
-
 public class Machine {
     public static int[] change(int money, int price) {
+        if (money < price) {
+            throw new RuntimeException("Нищеброды!");
+        }
+
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        for (int i = 0; i < coins.length; i++){
-            if (price > money){
-                coins[i] = size++;
+        int currentCoin = 0;
+
+        int changeTotal = money - price;
+        while (changeTotal > 0) {
+            currentCoin = 0;
+            for (int i = 0; i < coins.length; i++) {
+                if (coins[i] <= changeTotal) {
+                    currentCoin = coins[i];
+                    rsl[size++] = currentCoin;
+                    break;
+                }
             }
+            changeTotal -= currentCoin;
         }
+
         return Arrays.copyOf(rsl, size);
     }
 }
