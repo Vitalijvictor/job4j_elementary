75ab3594c373e6edd9c19f1811e783b84a3b4169
VitalijVictor <vitalijvictor@gmail.com>
2021-05-23 20:37:41 +0200

    Max
    [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,7 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-    	return left > right ? left : right;
+    public static int max(int a, int b) {
+    	return a > b ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+        int tmp = max(a, b);
+        return max(tmp, c);
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        int tmp = max(a, b);
+        int tmp1 = max(c, d);
+        return max(tmp, tmp1);
     }
 }
